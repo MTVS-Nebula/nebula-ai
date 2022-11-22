32ec1549ac9e766ccc47e12867ebef2959ba2153
ktaehyun <86669008+ktaehyun@users.noreply.github.com>
2022-11-22 21:34:06 +0900

    Update data_preprocessing.py

--- LogData/data_preprocessing.py
+++ LogData/data_preprocessing.py
@@ -5,11 +5,11 @@ from datetime import datetime
 class Databases():
     def __init__(self):
         self.db = psycopg2.connect(
-            host='nebula-rds.cobnraaxcbeh.ap-northeast-2.rds.amazonaws.com',
-            dbname='nebula_db',
-            user='nebula_net',
-            password='mtvs1130',
-            port=5432
+            host='*****',
+            dbname='*****',
+            user='*****',
+            password='*****',
+            port=*****
         )
         self.cursor = self.db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
