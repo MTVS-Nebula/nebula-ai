333d14aba4c6f11fc239d109629ee77ed003f28c
ktaehyun <k_taehyun94@naver.com>
2022-12-14 00:02:03 +0900

    PyCodeEdit

--- Embadding_Inference/append_coordinate_update/append_inference.py
+++ Embadding_Inference/append_coordinate_update/append_inference.py
@@ -69,21 +69,15 @@ class defaultRead(Databases):
 
         return result
 
-try:
-    dread = defaultRead()
-    allAppendId = dread.default_readDB()
+dread = defaultRead()
+allAppendId = dread.default_readDB()
 
-    # 새로 추가한 값의 ID 리스트
-    allDefaultId = []
-    while allAppendId:
-        now = allAppendId.pop(0)['id']
-        allDefaultId.append(now)
-
-    # 가장 처음 추가된 값의 ID
-    newAppendId = allDefaultId.pop(0)
-except:
-    allDefaultId = [11]
-    newAppendId = 11
+# 새로 추가한 값의 ID 리스트
+allDefaultId = []
+while allAppendId:
+    now = allAppendId.pop(0)['id']
+    allDefaultId.append(now)
+allDefaultid = str(tuple(allDefaultId))
 
 
 class CRUD(Databases):
@@ -93,8 +87,8 @@ class CRUD(Databases):
             FROM skyisland.tbl_sky_island AS sky
                 INNER JOIN avatar.tbl_avatar_tag AS avttag
                 ON sky.avatar_id = avttag.avatar_id
-                WHERE sky.id = {}
-        """.format(newAppendId)
+                WHERE sky.id in {}
+        """.format(allDefaultid)
         self.cursor.execute(sql)
         result = self.cursor.fetchall()
         return result
@@ -107,8 +101,8 @@ class CRUD(Databases):
                     ON sky.avatar_id = avt.id
                 INNER JOIN file.tbl_atc AS atc
                     ON avt.image_id = atc.id
-                WHERE sky.id = {}
-        """.format(newAppendId)
+                WHERE sky.id in {}
+        """.format(allDefaultid)
         self.cursor.execute(sql)
         result = self.cursor.fetchall()
         return result
@@ -121,9 +115,9 @@ class CRUD(Databases):
         except Exception as e:
             print(" update DB err", e)
 
-    def updateDB_skyIslandCoord(self, schema, table, skyIslandId, k1, k2, pc1, pc2, pc3):
+    def updateDB_skyIslandCoord(self, schema, table, skyIslandId, k1, k2, k3, k4, pc1, pc2, pc3):
         sql1 = f" UPDATE {schema}.{table} SET "
-        sql2 = f"id={skyIslandId}, keyword1='{k1}', keyword2='{k2}', pc1={pc1}, pc2={pc2}, pc3={pc3} "
+        sql2 = f"id={skyIslandId}, keyword1='{k1}', keyword2='{k2}', keyword3='{k3}', keyword4='{k4}', pc1={pc1}, pc2={pc2}, pc3={pc3} "
         sql3 = f"WHERE id = {skyIslandId}"
         sql = sql1 + sql2 + sql3
 
@@ -133,11 +127,9 @@ class CRUD(Databases):
         except Exception as e:
             print(" update DB err", e)
 
-
-class TEMP(Databases):
-    def readDB_all_tags(self):
+    def readDB_all_tag(self):
         sql = """
-            SELECT (sky.id, avttag.content) 
+            SELECT (sky.id, avttag.content)
             FROM skyisland.tbl_sky_island AS sky
                 INNER JOIN avatar.tbl_avatar_tag AS avttag
                 ON sky.avatar_id = avttag.avatar_id
@@ -146,9 +138,9 @@ class TEMP(Databases):
         result = self.cursor.fetchall()
         return result
 
-    def readDB_all_image_urls(self):
+    def readDB_all_image_url(self):
         sql = """
-            SELECT (sky.id, atc.saved_path) 
+            SELECT (sky.id, atc.saved_path)
             FROM skyisland.tbl_sky_island AS sky
                 INNER JOIN avatar.tbl_avatar AS avt
                     ON sky.avatar_id = avt.id
@@ -163,10 +155,10 @@ class TEMP(Databases):
 def umap_fit():
     umap_haversine = umap.UMAP(output_metric='haversine', random_state=42)
 
-    temp = TEMP()
+    temp = CRUD()
 
-    result_tag = temp.readDB_all_tags()
-    result_img = temp.readDB_all_image_urls()
+    result_tag = temp.readDB_all_tag()
+    result_img = temp.readDB_all_image_url()
 
     req = {}
     for row in result_tag:
@@ -222,77 +214,80 @@ def umap_fit():
 
     return umap_haversine
 
+if allDefaultid != '()':
+    umap_haversine = umap_fit()
 
-umap_haversine = umap_fit()
+    crud = CRUD()
 
+    result_tag = crud.readDB_all_tags()
+    result_img = crud.readDB_all_image_urls()
 
-crud = CRUD()
+    req = {}
+    for row in result_tag:
+        skyid = int(row['row'].split(',')[0][1:])
+        tag = row['row'].split(',')[1][:-1]
+        if skyid in req.keys():
+            req[skyid]['tag'].append(tag)
+        else:
+            req[skyid] = {'tag': [], 'image_url': ''}
+            req[skyid]['tag'].append(tag)
 
-result_tag = crud.readDB_all_tags()
-result_img = crud.readDB_all_image_urls()
+    for row in result_img:
+        skyid = int(row['row'].split(',')[0][1:])
+        url = row['row'].split(',')[1][:-1]
+        if skyid in req.keys():
+            req[skyid]['image_url'] = url
 
-req = {}
-for row in result_tag:
-    skyid = int(row['row'].split(',')[0][1:])
-    tag = row['row'].split(',')[1][:-1]
-    if skyid in req.keys():
-        req[skyid]['tag'].append(tag)
-    else:
-        req[skyid] = {'tag': [], 'image_url': ''}
-        req[skyid]['tag'].append(tag)
+    reqIds = []
+    image_text_features = []
+    keywords1, keywords2, keywords3, keywords4 = [], [], [], []
 
-for row in result_img:
-    skyid = int(row['row'].split(',')[0][1:])
-    url = row['row'].split(',')[1][:-1]
-    if skyid in req.keys():
-        req[skyid]['image_url'] = url
+    for k, v in req.items():
+        skyIslandId = k
+        tag1, tag2, tag3, tag4 = v['tag']
+        imageUrl = v['image_url']
 
-reqIds = []
-image_text_features = []
-keywords1, keywords2 = [], []
+        img = preprocess(Image.open(requests.get(imageUrl, stream=True).raw)).unsqueeze(0)
 
-for k, v in req.items():
-    skyIslandId = k
-    tag1, tag2, tag3, tag4 = v['tag']
-    imageUrl = v['image_url']
+        tag1_en = dict_cat1[tag1]
+        tag2_en = dict_cat2[tag2]
+        preset_keyword = tag1_en + ' ' + tag2_en
+        text_tokens = clip.tokenize(preset_keyword)
+        free_keyword = tag3 + ' ' + tag4
 
-    img = preprocess(Image.open(requests.get(imageUrl, stream=True).raw)).unsqueeze(0)
+        with torch.no_grad():
+            image_features = model.encode_image(img).float()
+            text_cat_features = model.encode_text(text_tokens).float()
+            text_free_features = model_multi_lang.forward(free_keyword, tokenizer).float()
 
-    tag1_en = dict_cat1[tag1]
-    tag2_en = dict_cat2[tag2]
-    preset_keyword = tag1_en + ' ' + tag2_en
-    text_tokens = clip.tokenize(preset_keyword)
-    free_keyword = tag3 + ' ' + tag4
+        image_text_feature = torch.cat([image_features, text_cat_features, text_free_features], dim=1).squeeze(0)  # image 512 + text_cat 512 + text_free 512 = 1536
+        image_text_feature = image_text_feature.detach().tolist()
+        image_text_features.append(image_text_feature)
+        del image_text_feature, image_features, text_cat_features, text_free_features
 
-    with torch.no_grad():
-        image_features = model.encode_image(img).float()
-        text_cat_features = model.encode_text(text_tokens).float()
-        text_free_features = model_multi_lang.forward(free_keyword, tokenizer).float()
+        reqIds.append(int(skyIslandId))
+        keywords1.append(tag1)
+        keywords2.append(tag2)
+        keywords3.append(tag3)
+        keywords4.append(tag4)
 
-    image_text_feature = torch.cat([image_features, text_cat_features, text_free_features], dim=1).squeeze(
-        0)  # image 512 + text_cat 512 + text_free 512 = 1536
-    image_text_feature = image_text_feature.detach().tolist()
-    image_text_features.append(image_text_feature)
-    del image_text_feature, image_features, text_cat_features, text_free_features
+    # UMAP
+    sphere_coord = umap_haversine.transform(image_text_features)
+    del image_text_features
 
-    reqIds.append(int(skyIslandId))
-    keywords1.append(tag1)
-    keywords2.append(tag2)
-
-# UMAP
-sphere_coord = umap_haversine.transform(image_text_features)
-del image_text_features
-
-x_coord = np.sin(sphere_coord[:, 0]) * np.cos(sphere_coord[:, 1]) * 2.0
-y_coord = np.sin(sphere_coord[:, 0]) * np.sin(sphere_coord[:, 1]) * 2.0
-z_coord = np.cos(sphere_coord[:, 0]) * 2.0
+    x_coord = np.sin(sphere_coord[:, 0]) * np.cos(sphere_coord[:, 1]) * 2.0
+    y_coord = np.sin(sphere_coord[:, 0]) * np.sin(sphere_coord[:, 1]) * 2.0
+    z_coord = np.cos(sphere_coord[:, 0]) * 2.0
 
 
 @app.route('/')
 def root():
     # update database
-    for p1, p2, p3, i, k1, k2 in zip(x_coord, y_coord, z_coord, reqIds, keywords1, keywords2):
-        crud.updateDB_skyIslandCoord('skyisland', 'tbl_sky_island_coordinate', int(i), k1, k2, p1, p2, p3)
+    try:
+        for p1, p2, p3, i, k1, k2, k3, k4 in zip(x_coord, y_coord, z_coord, reqIds, keywords1, keywords2, keywords3, keywords4):
+            crud.updateDB_skyIslandCoord('skyisland', 'tbl_sky_island_coordinate', int(i), k1, k2, k3, k4, p1, p2, p3)
+    except:
+        pass
 
     return 'APPEND UPDATE COMPLETE !'
 
